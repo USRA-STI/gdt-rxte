f8bab33c92d9da8510e427ea0b6c2162bdd62f58
Colleen A. Wilson-Hodge <cwilsonh@Colleens-MacBook-Pro-2.local>
2023-12-19 11:20:16 -0600

    added hdulist code to phaii.py

--- src/gdt/missions/rxte/phaii.py
+++ src/gdt/missions/rxte/phaii.py
@@ -1,4 +1,31 @@
-#Make Phaii for RXTE ASM data
+# CONTAINS TECHNICAL DATA/COMPUTER SOFTWARE DELIVERED TO THE U.S. GOVERNMENT WITH UNLIMITED RIGHTS
+#
+# Contract No.: CA 80MSFC17M0022
+# Contractor Name: Universities Space Research Association
+# Contractor Address: 7178 Columbia Gateway Drive, Columbia, MD 21046
+#
+# Copyright 2017-2022 by Universities Space Research Association (USRA). All rights reserved.
+#
+# Developed by: William Cleveland and Adam Goldstein
+#               Universities Space Research Association
+#               Science and Technology Institute
+#               https://sti.usra.edu
+#
+# Developed by: Daniel Kocevski
+#               National Aeronautics and Space Administration (NASA)
+#               Marshall Space Flight Center
+#               Astrophysics Branch (ST-12)
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License
+# is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing permissions and limitations under the
+# License.
+##Make Phaii for RXTE ASM data
 # when defining in a new instrument module, do a relative import instead:
 #from ..core.data_primitives import Ebounds, Gti, TimeEnergyBins
 #from ..core.phaii import Phaii
@@ -8,14 +35,16 @@ from astropy.io import ascii
 import numpy as np
 import os
 
+__all__ = ['RxtePhaiiNoHeaders']
+
 class RxtePhaiiNoHeaders(Phaii):
     def __init__(self, *args):            
         super(RxtePhaiiNoHeaders, self).__init__(*args)
         self._detector = None
     
-    def write(self, dirname, filename=None):
-        #writing the RXTEPhaii is not yet implemented.
-        raise NotImplementedError
+#    def write(self, dirname, filename=None):
+#        #writing the RXTEPhaii is not yet implemented.
+#        raise NotImplementedError
     
     @property
     def detector(self):
@@ -90,3 +119,111 @@ class RxtePhaiiNoHeaders(Phaii):
             print (filename,' not found.')
             return
 
+    def _build_hdulist(self):
+
+        # create FITS and primary header
+        hdulist = fits.HDUList()
+#        primary_hdu = fits.PrimaryHDU(header=self.headers['PRIMARY'])
+        primary_hdu = fits.PrimaryHDU()
+#       for key, val in self.headers['PRIMARY'].items():
+ #           primary_hdu.header[key] = val
+        hdulist.append(primary_hdu)
+        
+        # the ebounds extension
+        ebounds_hdu = self._ebounds_table()
+        hdulist.append(ebounds_hdu)
+        
+        # the spectrum extension
+        spectrum_hdu = self._spectrum_table()
+        hdulist.append(spectrum_hdu)        
+        
+        # the GTI extension
+        gti_hdu = self._gti_table()
+        hdulist.append(gti_hdu)
+        
+        return hdulist
+        
+#    def _build_headers(self, trigtime, tstart, tstop, num_chans):
+        
+#        headers = self.headers.copy()
+#        for hdu in headers:
+#            hdu['TSTART'] = tstart
+#            hdu['TSTOP'] = tstop
+#            try:
+#                hdu['DETCHANS'] = num_chans
+#            except:
+#                pass
+#            if trigtime is not None:
+#                hdu['TRIGTIME'] = trigtime
+#        
+#        return headers
+    
+    def _ebounds_table(self):
+        chan_col = fits.Column(name='CHANNEL', format='1I', 
+                               array=np.arange(self.num_chans, dtype=int))
+        emin_col = fits.Column(name='E_MIN', format='1E', unit='keV', 
+                               array=self.ebounds.low_edges())
+        emax_col = fits.Column(name='E_MAX', format='1E', unit='keV', 
+                               array=self.ebounds.high_edges())
+        
+#        hdu = fits.BinTableHDU.from_columns([chan_col, emin_col, emax_col], 
+#                                            header=self.headers['EBOUNDS'])
+        hdu = fits.BinTableHDU.from_columns([chan_col, emin_col, emax_col])
+#        for key, val in self.headers['EBOUNDS'].items():
+#            hdu.header[key] = val
+
+        return hdu
+
+    def _spectrum_table(self):
+        tstart = np.copy(self.data.tstart)
+        tstop = np.copy(self.data.tstop)
+        if self.trigtime is not None:
+            tstart += self.trigtime
+            tstop += self.trigtime
+        
+        counts_col = fits.Column(name='COUNTS', 
+                                 format='{}I'.format(self.num_chans), 
+                                 bzero=32768, bscale=1, unit='count',
+                                 array=self.data.counts)
+        expos_col = fits.Column(name='EXPOSURE', format='1E', unit='s', 
+                                array=self.data.exposure)
+#        qual_col = fits.Column(name='QUALITY', format='1I', 
+#                               array=self.data.quality)
+        time_col = fits.Column(name='TIME', format='1D', unit='s', 
+                               bzero=self.trigtime, array=tstart)
+        endtime_col = fits.Column(name='ENDTIME', format='1D', unit='s', 
+                                  bzero=self.trigtime, array=tstop)
+#        hdu = fits.BinTableHDU.from_columns([counts_col, expos_col, qual_col, 
+#                                             time_col, endtime_col], 
+#                                            header=self.headers['SPECTRUM'])
+        hdu = fits.BinTableHDU.from_columns([counts_col, expos_col, qual_col, 
+                                             time_col, endtime_col])
+
+#        for key, val in self.headers['SPECTRUM'].items():
+#            hdu.header[key] = val
+#        hdu.header.comments['TZERO1'] = 'offset for unsigned integers'
+#        hdu.header.comments['TSCAL1'] = 'data are not scaled'
+#        hdu.header.comments['TZERO4'] = 'Offset, equal to TRIGTIME'
+#        hdu.header.comments['TZERO5'] = 'Offset, equal to TRIGTIME'
+        return hdu
+
+    def _gti_table(self):
+        tstart = np.array(self.gti.low_edges())
+        tstop = np.array(self.gti.high_edges())
+        if self.trigtime is not None:
+            tstart += self.trigtime
+            tstop += self.trigtime
+
+        start_col = fits.Column(name='START', format='1D', unit='s', 
+                                bzero=self.trigtime, array=tstart)
+        stop_col = fits.Column(name='STOP', format='1D', unit='s', 
+                                bzero=self.trigtime, array=tstop)
+#        hdu = fits.BinTableHDU.from_columns([start_col, stop_col], 
+#                                            header=self.headers['GTI'])
+        hdu = fits.BinTableHDU.from_columns([start_col, stop_col])
+        
+#        for key, val in self.headers['GTI'].items():
+#            hdu.header[key] = val        
+#        hdu.header.comments['TZERO1'] = 'Offset, equal to TRIGTIME'
+#        hdu.header.comments['TZERO2'] = 'Offset, equal to TRIGTIME'
+        return hdu
