0a82d089e22659e5d531c909f628c4c02298213a
Colleen A. Wilson-Hodge <cwilsonh@Colleens-MBP-2.ndc.nasa.gov>
2023-12-22 13:57:11 -0600

    added fits.open to phaii.py

--- src/gdt/missions/rxte/phaii.py
+++ src/gdt/missions/rxte/phaii.py
@@ -54,7 +54,7 @@ class RxtePhaiiNoHeaders(Phaii):
         return self._detector
     
     @classmethod
-    def open_ascii(cls, filename, detector, t0):
+    def open(cls, filename, detector, t0):
         #inputs
         #filename (str) - name of dwell file including path
         #detector (str) - detector name: ssc0, ssc1, or ssc2
@@ -126,11 +126,12 @@ class RxtePhaiiNoHeaders(Phaii):
         #inputs
         #filename (str) - name of dwell file including path
                 			    
-        obj = super().open(filename, **kwargs)
         #check if filename exists	
         if os.path.isfile(filename):
             #if the filename exisits then open it
+	    obj = fits.open(filename)
             obj._filename = filename
+	    
        
             # get the headers
 #            hdrs = [hdu.header for hdu in obj.hdulist]
